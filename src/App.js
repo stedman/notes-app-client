20b66a2291720a2d801f1a250021d311e5704928
Steve Stedman <steve@stedman.dev>
2020-01-28 10:58:51 -0600

    Complete tutorial

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-  return <div>client-side app</div>;
-}
-
-export default App;
---
+++ src/environment.js
@@ -0,0 +1,24 @@
+import {
+  Environment, Network, RecordSource, Store,
+} from 'relay-runtime';
+
+function fetchQuery(operation, variables) {
+  // eslint-disable-next-line no-undef
+  return fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then((response) => response.json());
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
